bdffba363f03e75a10a24bd0b2d903e44dd96f7e
curtiss-504 <96351719+curtiss-504@users.noreply.github.com>
2023-11-13 17:38:40 -0800

    Update projet 1.py

--- giff/projet 1.py
+++ giff/projet 1.py
@@ -1,15 +1,16 @@
+
 from moviepy.editor import VideoFileClip
 
-def video_to_gif(input_path, output_path, fps=10):
-  
+def video_to_gif(input_path, output_path, fps=8):
+  # Nou kreye objet video ah
     clip = VideoFileClip(input_path)
 
     clip.write_gif(output_path, fps=fps)
-
+#itilizasyon fonksyon ahn 
 input_video_path = 'videyo.mp4'
 
 output_gif_path = 'animasyon.gif'
 
-frame_rate = 10
+frame_rate = 8
 
 video_to_gif(input_video_path, output_gif_path, frame_rate)
